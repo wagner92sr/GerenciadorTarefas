a9e5ee9a39bf855f8a87d6df8100af7c67e2a002
Wagner Rodrigues <wagnersr92@gmail.com>
2018-06-10 21:37:30 -0300

    criacao edit e partial. Ajuste controller para setar id e realizar o destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+ 
    def index
       @tasks = Task.all
    end
@@ -38,6 +40,11 @@ class TasksController < ApplicationController
 
    private
    def task_params
-      params.require(:task).permit(:name, :details, :due_date, :category_id, done)
+      params.require(:task).permit(:name, :details, :due_date, :category_id, :done)
    end
+
+   def set_task
+      @task = Task.find(params[:id])
+    end
+
 end
